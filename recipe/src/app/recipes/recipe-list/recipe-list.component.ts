9a7ba70359201ffdbdf6e3bf6c3d99c6faa2c7cb
html-helpers <hirenrtalsaniya@gmail.com>
2018-09-21 11:37:02 +0530

    Recipe Appllication code added.
    Added my first angular application.

---
+++ recipe/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe', 'This is just description', 'http://www.trandynow.com/wp-content/uploads/2018/04/recipe.jpg'),
+    new Recipe('Test Recipe 2', 'This is just description 2', 'http://www.trandynow.com/wp-content/uploads/2018/04/recipe.jpg')
+  ];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
+  }
+
+}
